6722f16b0eb59b2b9056a43bce71fc1cdaa546ec
Salil Saurav <salilsaurav.work@gmail.com>
2024-06-25 12:59:31 +0530

    color-change-code refactored

--- Change-color-inview/app.js
+++ Change-color-inview/app.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
         window.addEventListener("scroll", () => {
             let activeFound = false; // Flag to indicate if an active item has been found
 
-            // Remove 'active' class from all 'li' elements before checking visibility
             tabLinks.querySelectorAll("li").forEach((item) => {
                 item.classList.remove("active");
             });
@@ -22,19 +21,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 // Only proceed if no active item has been found yet
                 if (!activeFound) {
                     const id = item
-                        .querySelector("a")
-                        .getAttribute("href")
-                        .replace("#", ""); // Get the target element ID from the 'href' attribute
-                    const target = document.getElementById(id); // Get the target element by its ID
+                            .querySelector("a")
+                            .getAttribute("href")
+                            .replace("#", ""),
+                        target = document.getElementById(id);
 
                     // If the target element exists
                     if (target) {
-                        const rect = target.getBoundingClientRect(); // Get the bounding rectangle of the target element
-                        const isVisible =
-                            rect.top < window.innerHeight - offset &&
-                            rect.bottom >= offset; // Check if the element is visible with the specified offset
+                        const rect = target.getBoundingClientRect(),
+                            isVisible =
+                                rect.top < window.innerHeight - offset &&
+                                rect.bottom >= offset; // Check if the element is visible with the specified offset
 
-                        // If the target element is visible in the viewport with offset
                         if (isVisible) {
                             item.classList.add("active"); // Add 'active' class to the 'li' element
                             activeFound = true; // Set the flag to true as an active item is found
@@ -46,13 +44,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Add click event listeners to each 'li' element to manage active class
         tabLinks.querySelectorAll("li").forEach((item) => {
+            const anchor = item.querySelector("a");
+
+            anchor &&
+                anchor.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    const id = anchor.getAttribute("href").replace("#", ""),
+                        target = document.getElementById(id);
+
+                    target && customScroll(target, 100);
+                });
+
             item.addEventListener("click", (evt) => {
                 // Remove 'active' class from all 'li' elements
                 tabLinks.querySelectorAll("li").forEach((el) => {
                     el.classList.remove("active");
                 });
 
-                // Add 'active' class to the clicked 'li' element
                 evt.currentTarget.classList.add("active");
             });
         });
